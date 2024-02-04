7cf154c04e0b30ba8e6d4495e200d7cbf547bac6
abanoubjohnny <abanoub.sman1@outlook.com>
2024-02-03 19:47:17 -0500

    initialize project

---
+++ Contoso Crafts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Contoso_Crafts.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
